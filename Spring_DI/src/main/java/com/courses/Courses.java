1666c49cb43b88a0a96093be97f2addd4bbdc143
Darshak Goswami <your_email@example.com>
2025-11-17 12:24:25 +0530

    added

---
+++ Spring_DI/src/main/java/com/courses/Courses.java
@@ -0,0 +1,26 @@
+package com.courses;
+
+import java.util.List;
+
+import com.student.Student;
+
+public class Courses {
+private int course_Id;
+private String courseName;
+public int getCourse_Id() {
+	return course_Id;
+}
+public void setCourse_Id(int course_Id) {
+	this.course_Id = course_Id;
+}
+public String getCourseName() {
+	return courseName;
+}
+public void setCourseName(String courseName) {
+	this.courseName = courseName;
+}
+@Override
+public String toString() {
+	return "Courses [course_Id=" + course_Id + ", courseName=" + courseName + "]";
+}
+}
---
+++ Spring_DI/src/main/java/com/generics/Arraylist.java
@@ -0,0 +1,19 @@
+package com.generics;
+
+import java.util.List;
+
+public class Arraylist {
+private List<Object> list;
+
+public List<Object> getList() {
+	return list;
+}
+
+public void setList(List<Object> list) {
+	this.list = list;
+}
+public void DisplayList()
+{
+	System.out.println(list);
+}
+}
