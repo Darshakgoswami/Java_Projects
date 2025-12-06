5fec77960ef28e349577ee9d36d0a2c8f6924289
Darshak Goswami <your_email@example.com>
2025-12-06 12:24:30 +0530

    added

--- SpringImageAPI/src/main/java/com/example/SpringImageAPI/ImageUploadcontroller.java
+++ SpringImageAPI/src/main/java/com/example/SpringImageAPI/ImageUploadcontroller.java
@@ -1,13 +1,20 @@
 package com.example.SpringImageAPI;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -39,6 +46,31 @@ public class ImageUploadcontroller
 		Path filePath = uploadPath.resolve(filename);
 		Files.copy(file.getInputStream(),filePath,StandardCopyOption.REPLACE_EXISTING);
 		return filePath.toString();
+	}
+	
+	@GetMapping("/images/{filename}")
+	public ResponseEntity<Resource> getImage(@PathVariable String filename)
+	{
+	try
+	{
+		Path filePath = Paths.get(image).resolve(filename);
+		Resource resource = new UrlResource(filePath.toUri());
+		
+		if(resource.exists())
+		{
+			return ResponseEntity.ok()
+					.contentType(MediaType.IMAGE_JPEG)
+					.body(resource);
+		}
+		else
+		{
+			return ResponseEntity.notFound().build();
+		}
+	}
+		catch (MalformedURLException e)
+		{
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
 	}
 
 }
